adb9718a183441054c4652da3c6decd19d90cf14
damdadi93 <77727581+damdadi93@users.noreply.github.com>
2023-10-23 20:56:50 +0900

    Modfy push
    프로젝트 다시 푸시

---
+++ IU_StoneAge/Assets/Sangwoo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sangwoo : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("Sangwoo first push");
+    }
+
+}
