4b3838b6724664b9b4eaa1fa072f66149e6e32b7
sionKim <tldhswls123@naver.com>
2023-10-27 08:25:01 +0900

    mainc camera ex - ing

--- IU_StoneAge/Assets/Scripts/PlayerScript/PlayerController.cs
+++ IU_StoneAge/Assets/Scripts/PlayerScript/PlayerController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sion : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity!");
+        
     }
 
     // Update is called once per frame
