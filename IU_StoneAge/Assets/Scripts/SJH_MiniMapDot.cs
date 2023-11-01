75713ef69ddb2a87ba7c7c91289df5c010f8e2c2
JunOnJuly <wlgnstjd0717@gmail.com>
2023-11-01 20:24:48 +0900

    add map points

--- IU_StoneAge/Assets/Scripts/SJH_MiniMapDot.cs
+++ IU_StoneAge/Assets/Scripts/SJH_MiniMapDot.cs
@@ -9,13 +9,20 @@ public class SJH_MiniMapDot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, 30f, player.transform.position.z);
-        transform.rotation = Quaternion.Euler(0, 180, 0);
+        if (player.tag.Equals("Player"))
+        {
+            transform.rotation = player.transform.rotation;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
